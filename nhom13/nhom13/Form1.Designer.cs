7b9c1a22553be2b92bbdd37d5b850bfee2528bd5
Nhan <phanthanhnhan808@gmail.com>
2024-03-06 14:30:47 +0700

    password

--- nhom13/nhom13/Form1.Designer.cs
+++ nhom13/nhom13/Form1.Designer.cs
@@ -30,6 +30,8 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.txtUserName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textMatkhau = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label1
@@ -48,11 +50,29 @@
             this.txtUserName.Size = new System.Drawing.Size(165, 20);
             this.txtUserName.TabIndex = 1;
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(151, 108);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Mật Khẩu";
+            // 
+            // textMatkhau
+            // 
+            this.textMatkhau.Location = new System.Drawing.Point(237, 105);
+            this.textMatkhau.Name = "textMatkhau";
+            this.textMatkhau.Size = new System.Drawing.Size(165, 20);
+            this.textMatkhau.TabIndex = 3;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textMatkhau);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.txtUserName);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
@@ -66,6 +86,8 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textMatkhau;
     }
 }
 
--- nhom13/nhom13/Form1.cs
+++ nhom13/nhom13/Form1.cs
@@ -16,5 +16,10 @@ namespace nhom13
         {
             InitializeComponent();
         }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
