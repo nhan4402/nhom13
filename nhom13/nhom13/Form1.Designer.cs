e3387fcce2d334a61c790f2520a03fb2d5cafad4
Nhan <phanthanhnhan808@gmail.com>
2024-03-06 14:33:32 +0700

    commit

--- nhom13/nhom13/Form1.Designer.cs
+++ nhom13/nhom13/Form1.Designer.cs
@@ -39,9 +39,9 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(148, 55);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.Size = new System.Drawing.Size(77, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "UserName";
+            this.label1.Text = "tên đăng nhập";
             // 
             // txtUserName
             // 
@@ -55,9 +55,9 @@
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(151, 108);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.Size = new System.Drawing.Size(51, 13);
             this.label2.TabIndex = 2;
-            this.label2.Text = "Mật Khẩu";
+            this.label2.Text = "mật khẩu";
             // 
             // textMatkhau
             // 
